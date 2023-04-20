0f96cbf4ac94bda1b250c3610d546c6fe362483c
Kalapis Dorottya <kalapisd@gmail.com>
2023-04-20 14:51:09 +0200

    add genre controller

---
+++ src/main/java/com/example/homelibrary/contoller/GenreController.java
@@ -0,0 +1,35 @@
+package com.example.homelibrary.contoller;
+
+import com.example.homelibrary.DTO.GenreDTO;
+import com.example.homelibrary.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genre")
+public class GenreController {
+
+    private GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping
+    public List<GenreDTO> getAuthors() {
+        return genreService.getAllGenre();
+    }
+
+    @GetMapping("{id}")
+    public GenreDTO findGenreById(@PathVariable("id") long id){
+        return genreService.findGenreById(id);
+    }
+
+
+}
