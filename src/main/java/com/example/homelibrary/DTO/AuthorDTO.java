9228d9ac34b171d992ab4c56d10bc20e2ea75dcb
Kalapis Dorottya <kalapisd@gmail.com>
2023-04-24 18:25:23 +0200

    fix lombok annotations and add equals+hash

--- src/main/java/com/example/homelibrary/DTO/AuthorDTO.java
+++ src/main/java/com/example/homelibrary/DTO/AuthorDTO.java
@@ -1,12 +1,33 @@
 package com.example.homelibrary.DTO;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorDTO {
 
     private String name;
     private List<String> books;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AuthorDTO authorDTO)) return false;
+        return Objects.equals(getName(), authorDTO.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
 }
