24f907a6f3f69c58409a5ec657e867daaf483b24
Kalapis Dorottya <kalapisd@gmail.com>
2023-04-24 18:30:29 +0200

    fix lombok annotations + add equals and hash to genredto

--- src/main/java/com/example/homelibrary/DTO/GenreDTO.java
+++ src/main/java/com/example/homelibrary/DTO/GenreDTO.java
@@ -1,12 +1,31 @@
 package com.example.homelibrary.DTO;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GenreDTO {
 
     private String genre;
     private List<String> booksOfGenre;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GenreDTO genreDTO)) return false;
+        return Objects.equals(getGenre(), genreDTO.getGenre());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getGenre());
+    }
 }
