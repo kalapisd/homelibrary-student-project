e681417f9130cf7fc2b15112b2095e0a9c69b24f
Kalapis Dorottya <kalapisd@gmail.com>
2023-04-24 18:25:45 +0200

    remove unused query

--- src/main/java/com/example/homelibrary/repository/AuthorRepository.java
+++ src/main/java/com/example/homelibrary/repository/AuthorRepository.java
@@ -4,7 +4,6 @@ import com.example.homelibrary.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,6 +11,5 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     Optional<Author> findAuthorByName(String name);
 
-    List<Author> findAllByBooks_Id(Long bookId);
 
 }
