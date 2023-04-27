66446a16ceb423d1277762dadf212f7f2757eaac
Kalapis Dorottya <kalapisd@gmail.com>
2023-04-27 17:37:41 +0200

    rewrite methods to return response entity

--- src/main/java/com/example/homelibrary/contoller/AuthorController.java
+++ src/main/java/com/example/homelibrary/contoller/AuthorController.java
@@ -6,9 +6,12 @@ import com.example.homelibrary.DTO.commands.AuthorCommand;
 import com.example.homelibrary.service.AuthorService;
 import com.example.homelibrary.service.BookService;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,19 +40,29 @@ public class AuthorController {
         return bookService.findAllBooksOfAuthor(name);
     }
 
-    @GetMapping("/{name}")
-    public AuthorDTO findAuthorByName(@PathVariable("name") AuthorCommand command) {
-        return authorService.findAuthorByName(command);
+    @GetMapping("/findbyname/{name}")
+    public ResponseEntity<AuthorDTO> findAuthorByName(@PathVariable("name") AuthorCommand command) {
+        AuthorDTO authorDTO = authorService.findAuthorByName(command);
+        if (authorDTO == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(authorDTO);
+        }
     }
 
     @GetMapping("/{id}")
-    public AuthorDTO findAuthorById(@PathVariable("id") Long id) {
-        return authorService.findAuthorById(id);
+    public ResponseEntity<AuthorDTO> findAuthorById(@PathVariable("id") Long id) {
+        AuthorDTO authorDTO = authorService.findAuthorById(id);
+        if (authorDTO == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(authorDTO);
+        }
     }
-
     @PostMapping
-    public void save(@RequestParam("name") @Valid AuthorCommand authorCommand) {
-        authorService.saveAuthorByName(authorCommand);
+    public  ResponseEntity<AuthorDTO> saveAuthor(@RequestBody @Valid AuthorCommand authorCommand, BindingResult errors) {
+        if (errors.hasErrors()) return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(authorService.saveAuthorByName(authorCommand));
     }
 
 }
