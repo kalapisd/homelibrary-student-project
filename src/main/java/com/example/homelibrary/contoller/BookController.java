b98db33f9dfa5e327787b6ab8cb2ef938c38b124
Kalapis Dorottya <kalapisd@gmail.com>
2023-04-28 15:40:43 +0200

    add swagger to bookcontroller

--- src/main/java/com/example/homelibrary/contoller/BookController.java
+++ src/main/java/com/example/homelibrary/contoller/BookController.java
@@ -4,6 +4,9 @@ import com.example.homelibrary.DTO.BookDTO;
 import com.example.homelibrary.DTO.commands.APICommand;
 import com.example.homelibrary.DTO.commands.BookCommand;
 import com.example.homelibrary.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/books")
+@Tag(name = "Here you can make operations on books")
 public class BookController {
 
     private BookService bookService;
@@ -32,64 +36,86 @@ public class BookController {
     }
 
     @GetMapping
+    @Operation(summary = "Find all books",
+            description = "Here you can get all books stored in the database.")
     public List<BookDTO> getBooks() {
         return bookService.findAllBooks();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<BookDTO> findBookById(@PathVariable("id") Long id) {
+    @Operation(summary = "Find book by id",
+            description = "Here you can find a book by it's ID.")
+    public ResponseEntity<BookDTO> findBookById(
+            @Parameter(description = "Id of the book", example = "1")
+            @PathVariable("id") Long id) {
         BookDTO bookDTO = bookService.findBookById(id);
-
-        if (bookDTO == null) {
-            return ResponseEntity.notFound().build();
-        } else {
-            return ResponseEntity.ok(bookDTO);
-        }
+        return bookDTO == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(bookDTO);
     }
 
     @GetMapping("/findbytitle/{title}")
-    public ResponseEntity<BookDTO> findBookByTitle(@PathVariable("title") String title) {
+    @Operation(summary = "Find book by title",
+            description = "Here you can find a book by it's title.")
+    public ResponseEntity<BookDTO> findBookByTitle(
+            @Parameter(description = "Title of the book", example = "A Gyűrűk Ura")
+            @PathVariable("title") String title) {
         BookDTO bookDTO = bookService.findBookByTitle(title);
-
-        if (bookDTO == null) {
-            return ResponseEntity.notFound().build();
-        } else {
-            return ResponseEntity.ok(bookDTO);
-        }
+        return bookDTO == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(bookDTO);
     }
 
     @PostMapping("/manually")
+    @Operation(summary = "Manually save book to database",
+            description = "Here you can save book to database by sending a JSON with its parameters.")
     public ResponseEntity<BookDTO> saveManually(@RequestBody @Valid BookCommand command, BindingResult errors) {
         if (errors.hasErrors()) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(bookService.save(command));
     }
 
     @PostMapping("/fromAPI")
+    @Operation(summary = "Save book using GOOGLE API data",
+            description = "Here you can save book to database by getting book data from Google books API.")
     public ResponseEntity<BookDTO> saveFromApiData(@RequestBody @Valid APICommand command, BindingResult errors) {
         if (errors.hasErrors()) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(bookService.saveBookFomAPiDATA(command));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<BookDTO> updateBook(@PathVariable("id") Long id, @RequestBody @Valid BookCommand command, BindingResult errors) {
-        if (errors.hasErrors()) return ResponseEntity.badRequest().build();
+    @Operation(summary = "Update book",
+            description = "Here you can update book data in the database by giving its ID, and the data you want to be updated to.")
+    public ResponseEntity<BookDTO> updateBook(
+            @Parameter(description = "Id of the book", example = "1")
+            @PathVariable("id") Long id, @RequestBody @Valid BookCommand command, BindingResult errors) {
+        if (errors.hasErrors() || bookService.findBookById(id) == null) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(bookService.updateBook(id, command));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Long id) {
+    @Operation(summary = "Delete book from database",
+            description = "Here you can delete a book from the database by giving its ID.")
+    public void delete(
+            @Parameter(description = "Id of the book", example = "1")
+            @PathVariable("id") Long id) {
         bookService.deleteBook(id);
     }
 
     @PostMapping("/addauthor")
-    public void addAuthorToBook(@RequestParam("bookId") String bookId,
-                                @RequestParam("name") String name) {
+    @Operation(summary = "Add author to book",
+            description = "Here you can add author to a book, if it is missing by default.")
+    public void addAuthorToBook(
+            @Parameter(description = "book ID", example = "1")
+            @RequestParam("bookId") String bookId,
+            @Parameter(description = "name of the author", example = "Ernest Hemingway")
+            @RequestParam("name") String name) {
         bookService.addAuthorToBook(bookId, name);
     }
 
     @PostMapping("/addgenre")
-    public void addGenreToBook(@RequestParam("bookId") String bookId,
-                               @RequestParam("genre") String genre) {
+    @Operation(summary = "Add genre to book",
+            description = "Here you can add genre to a book, if it is missing by default.")
+    public void addGenreToBook(
+            @Parameter(description = "book ID", example = "1")
+            @RequestParam("bookId") String bookId,
+            @Parameter(description = "genre type", example = "LITERATURE")
+            @RequestParam("genre") String genre) {
         bookService.addGenreToBook(bookId, genre);
     }
 
