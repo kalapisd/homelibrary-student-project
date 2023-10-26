ec79f121ebba61bbd675438b1196f70da433bc03
kalapisd <kalapisd@gmail.com>
2023-10-26 16:52:27 +0200

    refactor BookMapper

--- src/main/java/com/example/homelibrary/mapper/BookMapper.java
+++ src/main/java/com/example/homelibrary/mapper/BookMapper.java
@@ -5,6 +5,7 @@ import com.example.homelibrary.entity.Book;
 import com.example.homelibrary.DTO.commands.BookCommand;
 import com.example.homelibrary.DTO.BookDTO;
 import com.example.homelibrary.utils.utilsdata.VolumeInfo;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
@@ -42,6 +43,8 @@ public class BookMapper {
         if (book.getGenre() != null) {
             bookDTO.setGenre(String.valueOf(book.getGenre().getGenreType()));
         }
+        bookDTO.setCurrentRating(book.getCurrentRating());
+        bookDTO.setImage(Base64.encodeBase64String(book.getImage()));
         return bookDTO;
     }
 }
